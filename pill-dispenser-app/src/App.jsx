877b0aa54a779a6dda6d9019be9cbb0cba2ee39b
Jackie Mac Hale <machalejackie@gmail.com>
2024-10-18 18:48:48 -0400

    Add some basic functionality to login page

--- pill-dispenser-app/src/App.jsx
+++ pill-dispenser-app/src/App.jsx
@@ -1,10 +1,20 @@
 import './App.css';
 import * as React from 'react';
+import { useState } from 'react';
 import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 
 export default function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = (event) => {
+    if (username === "jackie" && password === "1234") {
+      console.log("Logged in!");
+    }
+  };
+
   return (
     // <div className="App">
     //   <header className="App-header">
@@ -22,11 +32,23 @@ export default function App() {
     //     </a>
     //   </header>
     // </div>
-    <Stack>
+    <Stack spacing={2}>
       <h1>Authorization</h1>
-      <TextField label="Username" variant="outlined" />
-      <TextField label="Password" variant="outlined" />
-      <Button variant="contained">Log In</Button>
+      <TextField label="Username" variant="outlined" value={username}
+        onChange={(event) => {
+          console.log(username);
+          setUsername(event.target.value);
+        }}
+      />
+      <TextField label="Password" variant="outlined" value={password} type="password"
+        onChange={(event) => {
+          console.log(password);
+          setPassword(event.target.value);
+        }}
+      />
+      <Stack sx={{ alignItems: "center" }}>
+        <Button sx={{ width: "200px" }} variant="contained" onClick={login}>Log In</Button>
+      </Stack>
     </Stack>
   );
 }
