f67b4ce97c20a5180e7ccb8c7a0c19c32b32fc4a
Jackie Mac Hale <machalejackie@gmail.com>
2024-11-02 02:11:47 +0000

    Working scale firmware

--- pill-dispenser-app/src/firmware/scale.c
+++ pill-dispenser-app/src/firmware/scale.c
@@ -1,16 +1,34 @@
 #include <wiringPi.h>
 
+int power = 9;
+int units = 10;
+int tare1 = 22;
+int tare2 = 27;
+int weigh = 17;
+
 int main(void)
 {
     wiringPiSetupGpio();
-    pinMode(17, OUTPUT);
-    int delayNum = 50;
-    int period = 100;
+    pinMode(weigh, OUTPUT);
+    pinMode(tare2, OUTPUT);
+    pinMode(tare1, OUTPUT);
+    pinMode(units, OUTPUT);
+    pinMode(power, OUTPUT);
+    digitalWrite(weigh, LOW);
+    digitalWrite(tare2, LOW);
+    digitalWrite(tare1, LOW);
+    digitalWrite(units, LOW);
+    digitalWrite(power, HIGH);
+    delay(1000);
+    digitalWrite(power, LOW);
+    delay(10000);
+    int delayNum = 400;
+    int period = 1000;
     while (1)
     {
-        digitalWrite(17, HIGH);
+        digitalWrite(weigh, HIGH);
         delay(delayNum);
-        digitalWrite(17, LOW);
+        digitalWrite(weigh, LOW);
         delay(period - delayNum);
     }
 }
