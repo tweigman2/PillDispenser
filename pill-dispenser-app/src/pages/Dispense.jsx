fb36dc9d82df536128c9cdbeb9959742fcfa156e
Jackie Mac Hale <machalejackie@gmail.com>
2024-11-17 19:36:54 +0000

    Changed dispensing

--- pill-dispenser-app/src/pages/Dispense.jsx
+++ pill-dispenser-app/src/pages/Dispense.jsx
@@ -42,7 +42,7 @@ export default function Dispense({onDispenseClick, patientName, patientId, presc
             const dispensing_status = window.api.execCommand("src/firmware/i2c", ["r", MODULE_1_ADDR]);
             console.log(dispensing_status);
             if (parseInt(dispensing_status) === 1) {
-                if (output >= lowerBound * pillAmount && output <= upperBound * pillAmount) {
+                if (scaleWeight >= lowerBound * pillAmount && scaleWeight <= upperBound * pillAmount) {
                     onDispenseClick();
                 } else {
                     console.error("Pill weight is outside of the expected range! Double check that there are the right number of pills!");
